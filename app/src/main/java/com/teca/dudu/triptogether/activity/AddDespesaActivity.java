e07cdb63b2a44fa83864d17ef3c1da61fdfcde17
Eduardo <eduhdm0105@gmail.com>
2016-10-05 08:58:57 -0300

    tela add

--- app/src/main/java/com/teca/dudu/triptogether/activity/AddDespesaActivity.java
+++ app/src/main/java/com/teca/dudu/triptogether/activity/AddDespesaActivity.java
@@ -1,42 +1,66 @@
 package com.teca.dudu.triptogether.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.teca.dudu.triptogether.R;
+import com.teca.dudu.triptogether.adapter.QuemUsouAdapter;
 import com.teca.dudu.triptogether.dao.ItemDespesaDao;
+import com.teca.dudu.triptogether.dao.UsuarioDao;
 import com.teca.dudu.triptogether.model.ItemDespesa;
+import com.teca.dudu.triptogether.model.Usuario;
 
 import java.util.ArrayList;
 
 public class AddDespesaActivity extends AppCompatActivity {
     ItemDespesaDao novaDespesa;
+
     TextView descTV, valorTV;
-    Button addDespesaBtn;
+    Button addDespesaBtn, quemUsouBtn, quemPagouBtn;
     Spinner categoriasSpinner;
+    ListView listViewQuemUsou;
+
+    ArrayList<Usuario> usuarios;
+    UsuarioDao usuarioDao;
+
+    int[] ids_usuarios;
+    boolean[] checkUsuarios;
+    boolean visible = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_despesa);
-
-
+        usuarioDao = new UsuarioDao(this);
+        usuarios = new ArrayList<Usuario>();
+        usuarios  = usuarioDao.listaUsuariosDeUmaViagem(1);
+        ids_usuarios = new int[usuarios.size()];
+        for(int i=0;i<usuarios.size();i++){
+            ids_usuarios[i] = usuarios.get(i).get_id();
+        }
 
         descTV = (TextView)findViewById(R.id.desc_text);
         valorTV = (TextView)findViewById(R.id.valor_text);
         categoriasSpinner = (Spinner) findViewById(R.id.categorias_spinner);
         addDespesaBtn = (Button) findViewById(R.id.add_despesa_btn);
+        quemUsouBtn = (Button) findViewById(R.id.quemusou_btn);
+
+        listViewQuemUsou = (ListView) findViewById(R.id.listview_quemusou);
+       
+       // if(!visible)//nao mostra list view
+            //listViewQuemUsou.setVisibility(View.INVISIBLE);
 
 
         ArrayAdapter<String> adapterCategorias = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
-
-
         adapterCategorias.add("alimentacao");
         adapterCategorias.add("transporte");
         adapterCategorias.add("entretenimento");
@@ -60,6 +84,60 @@ public class AddDespesaActivity extends AppCompatActivity {
                 startActivity(intentmain);
             }
         });
+
+        quemUsouBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog("quem usou", v);
+            }
+        });
+    }
+    private void createDialog(String quem, View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+        CharSequence[] itens = new CharSequence[usuarios.size()];
+        checkUsuarios = new boolean[usuarios.size()];
+        for(int i =0;i<usuarios.size();i++){
+            itens[i] =(CharSequence) usuarios.get(i).getNome().toString();
+        }
+
+        builder.setTitle(quem)
+                .setMultiChoiceItems(itens, checkUsuarios, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        checkUsuarios[which] = isChecked;
+                    }
+                })
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        for(int i=0; i<usuarios.size();i++){
+                            if(checkUsuarios[i])
+                                visible=true;
+
+                        }
+                        if(visible)
+                            displayQuemUsou();
+                    }
+                })
+                .show();
+
+    }
+    public void displayQuemUsou(){
+        ArrayList<Usuario> quemUsou = new ArrayList<Usuario>();
+        for(int i=0; i<usuarios.size();i++){
+            if(checkUsuarios[i]){
+                quemUsou.add(usuarioDao.buscarUsuarioPorId(ids_usuarios[i]));
+
+            }
+        }
+        QuemUsouAdapter adapter = new QuemUsouAdapter(this, quemUsou);
+        listViewQuemUsou.setAdapter(adapter);
+        //listViewQuemUsou.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
     }
 }
---
+++ app/src/main/java/com/teca/dudu/triptogether/adapter/QuemUsouAdapter.java
@@ -0,0 +1,51 @@
+package com.teca.dudu.triptogether.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.teca.dudu.triptogether.R;
+import com.teca.dudu.triptogether.model.Usuario;
+
+import java.util.ArrayList;
+
+/**
+ * Created by DUDU on 04/10/2016.
+ */
+
+public class QuemUsouAdapter extends ArrayAdapter<Usuario>{
+    public QuemUsouAdapter(Context context, ArrayList<Usuario> usuarios){
+        super(context, 0, usuarios);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listViewItem = convertView;
+        if(listViewItem == null)
+            listViewItem = LayoutInflater.from(getContext()).inflate(R.layout.list_item_quemusou, parent, false);
+
+        Usuario currentUsuario = getItem(position);
+
+        TextView txtNomeUsuario = (TextView) listViewItem.findViewById(R.id.itemquemusou_nome);
+
+        txtNomeUsuario.setText(currentUsuario.getNome());
+
+
+        //TO DO
+        //Imagem do usuario
+
+        ImageView imgUsuario = (ImageView) listViewItem.findViewById(R.id.itemquemusou_img);
+
+        imgUsuario.setImageResource(R.drawable.ic_menu_gallery);
+
+        EditText editText = (EditText) listViewItem.findViewById(R.id.itemquemusou_edittxt);
+        editText.setText("0");
+        return listViewItem;
+
+    }
+}
